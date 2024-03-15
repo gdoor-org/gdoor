fbe1243a6582846b96e2ddcb563417e881a39d99
Alex <raal1400@stud.hs-coburg.de>
2024-03-15 14:00:50 +0100

    First try to include mqtt

--- firmware/esp32/gdoor/main.cpp
+++ firmware/esp32/gdoor/main.cpp
@@ -18,6 +18,7 @@
 #include "src/gdoor.h"
 #include <Wifi.h>
 #include <WiFiManager.h> 
+#include <MQTT.h>
 
 #define PIN_RX 12
 #define PIN_TX 25
@@ -26,33 +27,56 @@
 #define DEFAULT_WIFI_SSID       "GDoor"
 #define DEFAULT_WIFI_PASSWORD   "12345678"
 
+#define MQTT_TOPIC      "/gdoor"
+
+
 WiFiManager wifiManager;
+WiFiClient net;
+MQTTClient mqttClient;
+
+unsigned long lastMillis = 0;
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+}
 
 void setup() {
     Serial.begin(115200);
     Serial.setTimeout(1);
+    
+    Serial.println("GDOOR Setup start");
 
     wifiManager.autoConnect(DEFAULT_WIFI_SSID, DEFAULT_WIFI_PASSWORD);
 
-    Serial.println("GDOOR Setup start");
+    mqttClient.begin("192.168.10.249", 1884, net);
+    mqttClient.onMessage(messageReceived);
+
+    while (!mqttClient.connect("arduino")) {
+        Serial.print(".");
+        delay(1000);
+    } 
+
+    mqttClient.subscribe(MQTT_TOPIC);
+
     GDOOR::setup(PIN_TX, PIN_TX_EN, PIN_RX);
     Serial.println("GDOOR Setup done");
 }
 
 void loop() {
-    while(1) {
-        GDOOR::loop();
-        GDOOR_RX_DATA* rx_data = GDOOR::read();
-        if(rx_data != NULL) {
-            Serial.print("New data:\n");
-            Serial.print(*rx_data);
-        } else if (1 || !GDOOR::active()) { //Neither RX nor TX active,
-            if (Serial.available() > 0) { // let's check the serial port if something is in buffer
-                String serialstr = Serial.readString();
-                GDOOR::send(serialstr);
-                Serial.print("Send data:");
-                Serial.println(serialstr);
-            }
+    GDOOR::loop();
+    GDOOR_RX_DATA* rx_data = GDOOR::read();
+    if(rx_data != NULL) {
+        Serial.print("New data:\n");
+        Serial.print(*rx_data);
+        mqttClient.publish(MQTT_TOPIC, "1");
+    } else if (!GDOOR::active()) { //Neither RX nor TX active,
+        if (Serial.available() > 0) { // let's check the serial port if something is in buffer
+            String serialstr = Serial.readString();
+            GDOOR::send(serialstr);
+            Serial.print("Send data:");
+            Serial.println(serialstr);
         }
     }
+
 }
