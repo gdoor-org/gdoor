754614f82b41348d8f52457b629aed2d8e6c7f0a
Jakob Schröter <487046+jschroeter@users.noreply.github.com>
2024-06-22 00:58:01 +0200

    CI: push firmware to website repo

--- firmware/esp32/gdoor/merge_firmware.py
+++ firmware/esp32/gdoor/merge_firmware.py
@@ -1,10 +1,17 @@
 # see https://github.com/platformio/platform-espressif32/issues/1078#issuecomment-1636793463
 Import("env")
+import json
+import os
 
 APP_BIN = "$BUILD_DIR/${PROGNAME}.bin"
-MERGED_BIN = "$BUILD_DIR/${PROGNAME}_merged.bin"
+WEB_DIR = ".pio/build/GDOOR_ESP32MINI/web"
+MERGED_BIN = WEB_DIR + "/${PROGNAME}_merged.bin"
 BOARD_CONFIG = env.BoardConfig()
+MANIFEST_JSON = "manifest.json"
+MANIFEST_JSON_RESULT = WEB_DIR + "/manifest.json"
 
+if not os.path.exists(WEB_DIR):
+    os.makedirs(WEB_DIR)
 
 def merge_bin(source, target, env):
     # The list contains all extra images (bootloader, partitions, eboot) and
@@ -28,6 +35,14 @@ def merge_bin(source, target, env):
             + flash_images
         )
     )
+
+    # build web flasher manifest
+    with open(MANIFEST_JSON, 'r', encoding='utf-8') as json_file:
+        manifest = json.load(json_file)
+        manifest["version"] = env.get("GDOOR_VERSION", "dev")
+
+    with open(MANIFEST_JSON_RESULT, 'w', encoding='utf-8') as json_file:
+        json.dump(manifest, json_file)
 
 # Add a post action that runs esptoolpy to merge available flash images
 env.AddPostAction(APP_BIN, merge_bin)
